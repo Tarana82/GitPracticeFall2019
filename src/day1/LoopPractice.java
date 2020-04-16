58d55d78b1d94fa04542b0699144d0abfe7bb8ff
Tarana82 <tahmadova1982@gmail.com>
2020-04-16 15:58:16 -0500

    update

--- src/day1/LoopPractice.java
+++ src/day1/LoopPractice.java
@@ -2,9 +2,10 @@ package day1;
 
 public class LoopPractice {
     public static void main(String[] args) {
-        for (int i = 0; i < 10; i += 2) {
-            System.out.println("Counter :: " + i);
-            System.out.println("#####");
+        //some random code
+        for (int i=0; i <10; i+=2){
+            System.out.println("Counter :: "+i);
+            System.out.println("####");
             System.out.println("git is fun:(");
         }
     }
--- src/day1/StringPractice.java
+++ src/day1/StringPractice.java
@@ -5,4 +5,13 @@ public class StringPractice {
         System.out.println("Hello,world");
     }
 
+    public static class LoopPractice {
+        public static void main(String[] args) {
+            for (int i = 0; i < 10; i += 2) {
+                System.out.println("Counter :: " + i);
+                System.out.println("#####");
+                System.out.println("git is fun:(");
+            }
+        }
+    }
 }
